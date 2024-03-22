27068b1a37134ac48a4f2900659649c2086bc39d
galsynhaivska <galsynhaivska@gmail.com>
2024-03-22 13:21:59 +0200

    changed dz8.py

--- L4/dz8.py
+++ L4/dz8.py
@@ -5,17 +5,8 @@
 el_sum = 0
 lst = [0, 1, 7, 2, 4, 8]
 if len(lst) > 0:
-    for el in lst[::2]:
-        el_sum += el
+    el_sum = sum(lst[::2])
     el_sum *= lst[-1]
 print(el_sum)
 
-# Another way to get elements with even index
-el_sum = 0
-lst = [5]
-if len(lst) > 0:
-    for i in range(0, len(lst), 2):
-        print(i, lst[i])
-        el_sum += lst[i]
-    el_sum *= lst[-1]
-print(el_sum)
+
