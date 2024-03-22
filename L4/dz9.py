d27f7052a95ef018761c6c3a03c973679c89eecb
galsynhaivska <galsynhaivska@gmail.com>
2024-03-22 13:14:28 +0200

    changed dz9.py

--- L4/dz9.py
+++ L4/dz9.py
@@ -3,7 +3,7 @@ import random
 # [1, 1, 2, 1] == [1, 2, 2]
 # [6, 3, 7] == [6, 7, 3]
 # lst=[6, 3, 7]
-lst = [random.randint(1, 100) for i in range(random.randint(3, 11))]
+lst = [random.randint(1, 100) for i in range(random.randint(3, 10))]
 print(lst)
 new_list = lst[:3:2]
 new_list.append(lst[-2])
