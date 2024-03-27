ae18cefa29d7a2428c761062de7ced24d710f4fb
galsynhaivska <galsynhaivska@gmail.com>
2024-03-27 12:13:13 +0200

    changed dz12.py

--- L5/dz12.py
+++ L5/dz12.py
@@ -2,12 +2,12 @@ import string
 
 # my_str = 'Python Community'
 # my_str = 'i like python community!'
-my_str = ' Should, I. subscribe? Yes! '
+# my_str = ' Should, I. subscribe? Yes! '
+my_str = input('Input your string: ')
 my_str = my_str.title()
 for sign_ in string.punctuation:
     if sign_ in my_str:
         my_str = my_str.replace(sign_, '')
-lst = my_str.split()
-my_str = "#" + "".join(lst)
-hash_str = my_str[:138]
+my_str = "#" + "".join(my_str.split())
+hash_str = my_str[:140]
 print(hash_str)
