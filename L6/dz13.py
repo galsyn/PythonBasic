55cd609b4edd3faa664ad1ee827fde1811487649
galsynhaivska <galsynhaivska@gmail.com>
2024-03-28 13:13:17 +0200

    added dz13.py

---
+++ L6/dz13.py
@@ -0,0 +1,10 @@
+import string
+# "a-c" -> abc
+# "a-a" -> a
+# "s-H" -> stuvwxyzABCDEFGH
+# "a-A" -> abcdefghijklmnopqrstuvwxyzA
+user_string = input('Введіть через дефіс дві літери: ')
+index_1 = string.ascii_letters.find(user_string[0])
+index_2 = string.ascii_letters.find(user_string[-1])
+for i in range(index_1, index_2 + 1):
+    print(string.ascii_letters[i], sep="", end="")
