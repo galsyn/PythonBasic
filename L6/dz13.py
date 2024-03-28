36a547055a323390ec762d6948df0a491d90a29f
galsynhaivska <galsynhaivska@gmail.com>
2024-03-28 14:27:53 +0200

    changed dz13.py

--- L6/dz13.py
+++ L6/dz13.py
@@ -6,5 +6,6 @@ import string
 user_string = input('Введіть через дефіс дві літери: ')
 index_1 = string.ascii_letters.find(user_string[0])
 index_2 = string.ascii_letters.find(user_string[-1])
-for i in range(index_1, index_2 + 1):
-    print(string.ascii_letters[i], sep="", end="")
+# for i in range(index_1, index_2 + 1):
+#     print(string.ascii_letters[i], sep="", end="")
+print(string.ascii_letters[index_1:index_2 + 1])
