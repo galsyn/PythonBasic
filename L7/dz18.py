7e80d82d46baed4a0e94780b97ed4057f3002478
galsynhaivska <galsynhaivska@gmail.com>
2024-04-01 16:25:06 +0300

    added dz16-18.py

---
+++ L7/dz18.py
@@ -0,0 +1,11 @@
+def second_index(text, some_str):
+    if text.count(some_str) >= 2:
+        return text.find(some_str, text.find(some_str) + 1)
+
+assert second_index("sims", "s") == 3, 'Test1'
+assert second_index("find the river", "e") == 12, 'Test2'
+assert second_index("hi", "h") is None, 'Test3'
+assert second_index("Hello, hello", "lo") == 10, 'Test4'
+print('ОК')
+
+
